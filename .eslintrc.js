84a8957b628729d3d1f4a4f30d168ba7670eb92c
zhangzhzh <zhangzhzh@uino.com>
2021-09-02 16:16:52 +0800

    -mfeat: 添加stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,25 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  globals: {
+    THING: false,
+    CMAP: false,
+    uino: false,
+    spray: false,
+  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 4],
+    'vue/html-indent': ['error', 4],
+    'no-console': process.env.NODE_ENV === 'production' ? ["error", { allow: ["warn", "error"] }] : 'off',
+    'max-len': ['warn', { 'code': 500 }],
+    'vue/html-quotes': 'warn',
+    'no-param-reassign': ['error', { 'props': false }],
+    'camelcase': 0,
+    'no-mixed-operators': 'off',
+    'no-bitwise': 'off',
+    'no-unused-expressions': ['error', { 'allowShortCircuit': true }],
+    'no-restricted-globals': 'off',
+    'class-methods-use-this': 'off',
   },
   overrides: [
     {
